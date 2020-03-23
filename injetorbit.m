dedb7cb24e14a51259a4ee04f0b0d9d422f64e27
handong <31544492+handong1979@users.noreply.github.com>
2020-03-23 10:56:45 +0800

    入轨调相
    入轨调相的仿真

---
+++ injetorbit.m
@@ -0,0 +1,67 @@
+clear
+close all
+
+epoch = [2020,9,1,4,0,0];
+h0 = 1100;
+a = h0 + Re;
+kp = [a,0.001,85,120,60,0];
+F = 0.015;
+Isp = 1300;
+dm = -F/Isp/9.8;
+Mass = 200;
+s(1,:) = mexOrbitDyn('sat','init',[epoch,kp,Mass]);
+mexOrbitDyn('sat','SetForce',[8,1+2+4+8+16+32]);
+mexOrbitDyn('sat','SetAD',5); 
+mexOrbitDyn('sat','SetSRP',5);
+m(1,:) = mexOrbitDyn('sat','GetMean');
+
+%% 标称轨道
+kpstd = [Re + 1175,0.001,85,120,60,0];
+sstd(1,:) = mexOrbitDyn('std','init',[epoch,kpstd,Mass]);
+mexOrbitDyn('std','SetForce',[8,1+2+8+16]); % 不含大气与光压
+mstd(1,:) = mexOrbitDyn('std','GetMean');
+%% 
+step = 60;
+i = 1;
+%% 调相参数计算
+ut = 15; % 目标相位角，在200~360或0~160范围内
+u1 = ut - 196.7; % 目标相位角减去固定相位变化量
+if u1 < 0
+    u1 = u1 + 360;
+end
+du = duda(kpstd(1),a - kpstd(1))*deg; % 停泊轨道相位漂移率
+dt = u1/du * a2t(a); % 停泊轨道停泊时间
+
+%关点火时段,第一列为开始时间，第二列为结束时间，可以任意行数
+% fireoff = [0,1440;...
+%             1440*2,1440*3;...
+%            1440*4,1440*5];
+fireoff = [0,dt/step];
+
+for i = 2:(1440*10)
+    qbi = c2q(getcoi(s(i-1,2:7)));
+    if any(i>fireoff(:,1) & i<fireoff(:,2))        
+        s(i,:) = mexOrbitDyn('sat','step',step);
+    else
+        s(i,:) = mexOrbitDyn('sat','step',[step, qbi', F, 0, 0, dm]);
+    end
+    sstd(i,:) = mexOrbitDyn('std','step',step);
+    m(i,:) = mexOrbitDyn('sat','GetMean');
+    mstd(i,:) = mexOrbitDyn('std','GetMean');
+    if m(i,1) - Re > 1175
+        break;
+    end
+end
+mexOrbitDyn('sat','remove');
+mexOrbitDyn('std','remove');
+
+t = (s(:,1)-s(1,1));
+u = ewmu(s(:,3),s(:,6),s(:,7));
+ustd = ewmu(sstd(:,3),sstd(:,6),sstd(:,7));
+ue = limitpi(u-ustd)*deg;
+ue(ue<0) = ue(ue<0)+360;
+disp(ue(end));
+%%
+figure('Name','瞬根数'),plot6coe(t,s(:,2:7));
+figure('Name','平根数'),plot6coe(t,m);
+figure('Name','相位差'),plot(t,ue);
